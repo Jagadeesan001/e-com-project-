9763f219263aa443e37ca58011fa3c1cccd68c18
Jagadeesan001 <161101067+Jagadeesan001@users.noreply.github.com>
2024-02-24 09:42:36 +0530

    e-com add to github

---
+++ index.js
@@ -0,0 +1,14 @@
+// side-nav--------------------------------------------------
+var side_nav = document.querySelector(".side-nav-bar")
+
+function open_side_nav() {
+    side_nav.style.left = "0"
+
+}
+function close_side_nav() {
+    side_nav.style.left = "-50%"
+
+}
+
+
+
